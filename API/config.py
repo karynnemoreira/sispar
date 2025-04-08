a69b0e28c1d86f30cfae26aa824fba8c94790a1b
samuel-prata <samuel.silverio001@hotmail.com>
2025-04-08 15:05:11 -0300

    add: API pronta

--- API/config.py
+++ API/config.py
@@ -5,3 +5,5 @@ load_dotenv()
 
 class Config:
    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_LOCAL')
+   SECRET_KEY = 'bedigital'
+   JWT_SECRET_KEY = 'bedigital'
--- API/src/app.py
+++ API/src/app.py
@@ -5,9 +5,13 @@ from src.model import db
 from flask_migrate import Migrate
 from config import Config
 import os
+from flask_jwt_extended import JWTManager
 
 def create_app():
     app = Flask(__name__)
+    jwt = JWTManager(app)
+    from flask_cors import CORS
+    CORS(app, origins='*')
     
     app.register_blueprint(bp_employee)
     app.register_blueprint(bp_refund)
@@ -16,7 +20,12 @@ def create_app():
     app.config.from_object(Config)
     migrate = Migrate()
     
+    
+        
     db.init_app(app)
+    
+    with app.app_context():
+        db.create_all()
     migrate.init_app(app, db)
     
     return app
